a0448c101de3fef60ee72ddace813a350bdc9a1c
HamzaSami-gif <hamzasami123789@gmail.com>
2025-12-12 22:09:55 +0500

    Update main.cpp
    made addtask and calc end time functions

--- main.cpp
+++ main.cpp
@@ -19,3 +19,30 @@ struct Task {
 
 Task tasks[MAX_TASKS];
 int totalTasks = 0;
+
+int main(){
+    return 0;
+}
+
+void addTask() {
+    if(totalTasks >= MAX_TASKS) { cout << "Max tasks reached!\n"; return; }
+    cout << "Task Name: "; cin.getline(tasks[totalTasks].name, STR_LEN);
+    cout << "Start Time (e.g., 930): "; cin >> tasks[totalTasks].start;
+    cout << "Duration (min): "; cin >> tasks[totalTasks].duration;
+    cout << "Priority (1=High, 2=Med, 3=Low): "; cin >> tasks[totalTasks].priority;
+    cin.ignore();
+    calculateEndTimes();
+    totalTasks++;
+    cout << "Task added!\n";
+}
+
+// 2. Calculate end times
+void calculateEndTimes() {
+    for(int i = 0; i < totalTasks; i++) {
+        int h = tasks[i].start / 100;
+        int m = tasks[i].start % 100 + tasks[i].duration;
+        h += m / 60;
+        m %= 60;
+        tasks[i].end = h * 100 + m;
+    }
+}
